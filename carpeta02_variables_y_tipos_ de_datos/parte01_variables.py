5b1cdc737e01da1588179ba6b087f22ba7f0d832
lmpescobar <lmpes@tutanota.com>
2024-06-09 21:09:57 -0400

    Variables en Python

---
+++ carpeta02_variables_y_tipos_ de_datos/parte01_variables.py
@@ -0,0 +1,31 @@
+# Variables en Python
+
+"""
+Una variable en Python (y en la mayoría de los lenguajes de programación) es un nombre que se utiliza 
+para hacer referencia a un valor almacenado en la memoria de la computadora. Las variables permiten a los programadores 
+almacenar, modificar y acceder a datos durante la ejecución de un programa.
+
+Los nombres de las variables pueden contener letras (mayúsculas y minúsculas), números y guiones bajos (_), pero no pueden comenzar con un número.
+Los nombres de variables son sensibles a mayúsculas y minúsculas, es decir, variable y Variable son diferentes.
+
+"""
+
+# Crear variables y asignarles un valor
+texto = ("Master en Python")
+texto2 = ("con Luis")
+numero = 45
+decimal = 8.6
+
+# Mostrar el valor de las variables
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("-----------------------------")
+
+# Sustituir el valor de algunas variables
+numero = 77
+decimal = 8.9
+print(numero)
+print(decimal)
