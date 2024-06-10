73ca74b4feefb4083c1fa8c3817bf46f38b993c7
lmpescobar <lmpes@tutanota.com>
2024-06-09 21:23:56 -0400

    Concatenación

---
+++ carpeta02_variables_y_tipos_ de_datos/parte02_concatenar.py
@@ -0,0 +1,10 @@
+# Concatenar
+
+# Concatenación
+nombre = "Luis"
+apellidos = "Escobar"
+web = "LuisEscobarWeb.es"
+
+print(nombre + " " + apellidos + " - " + web)
+print(f"{nombre} {apellidos} - {web}")
+print("Hola me llamo {} {} y mi web es: {}".format(nombre,apellidos,web))
