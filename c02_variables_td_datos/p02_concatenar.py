40c2a8c88ccd84a15ff541839ac4993162e89125
lmpescobar <lmpes@tutanota.com>
2024-07-16 16:02:40 -0400

    Concatenar

---
+++ c02_variables_td_datos/p02_concatenar.py
@@ -0,0 +1,11 @@
+# Concatenar
+
+# Concatenación 
+
+nombre = "Luis"
+apellidos = "Escobar"
+web = "LuisEscobarWeb"
+
+print(nombre + " " + apellidos + " - " + web)
+print(f"{nombre} {apellidos} - {web}")
+print("Hola me llamo {} {} y mi web es: {}" .format(nombre, apellidos, web))
