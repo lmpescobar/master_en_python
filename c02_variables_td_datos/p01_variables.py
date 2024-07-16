acaaf87134ac44297d2ed25f9aae863bda635e34
lmpescobar <lmpes@tutanota.com>
2024-07-16 15:51:01 -0400

    Variables en Python

---
+++ c02_variables_td_datos/p01_variables.py
@@ -0,0 +1,27 @@
+# Variables en Python
+
+"""
+Una variable es un contenedor de información
+que dentro guardará un dato, sepueden crear
+muchas variables y que cada una tenga un dato distinto.
+"""
+
+# Crear variables  y asignarles un valor
+texto = "Máster en python"
+texto2 = "con Luis Escobar"
+numero = 45
+decimal = 6.7
+
+# Mostrar valor de las variables
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("----------------------------------------------------")
+
+# Sustituir el valor  de algunas variables
+numero = 77
+decimal = 8.9
+print(numero)
+print(decimal)
