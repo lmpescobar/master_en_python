8d13f794713d56393461e498b14b2d681ae0614c
lmpescobar <lmpes@tutanota.com>
2024-08-24 12:11:28 -0400

    Incremento y decremento

---
+++ c03_operadores/p03_incremento_decremento.py
@@ -0,0 +1,24 @@
+# Incremento y decremento
+
+year = 2021
+
+# Incremento
+year = year + 1
+year += 1
+
+# Decremento
+year = year - 1
+year -= 1
+
+# Pre-incremento
+year = 1 + year
+
+# Pre-decremento
+year = 1 - year
+
+"""
+year++
+year--
+--year
+++year
+"""
