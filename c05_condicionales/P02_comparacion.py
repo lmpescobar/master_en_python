5a8b04b2b90f2091a96edd655dac92264fb7fc45
lmpescobar <lmpes@tutanota.com>
2024-08-24 13:59:37 -0400

    Operadores de comparación

---
+++ c05_condicionales/P02_comparacion.py
@@ -0,0 +1,21 @@
+# Operadores de comparación
+
+"""
+== igual
+!= diferente
+< menor que
+> mayor que
+<= menor igual que
+>= mayor o igual que
+"""
+
+# Ejemplo 2
+print("########## Ejemplo 2 ##########")
+
+# year = 2020
+year = int(input("¿En que año estamos? "))
+
+if year >= 2021:
+    print("Estamos de 2021 en adelante !!")
+else:
+    print("Es un año anterior a 2021")
