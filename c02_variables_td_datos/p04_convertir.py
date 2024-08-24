45c2a0681b10c401098c6f11f91ae3bd5e6f9db8
lmpescobar <lmpes@tutanota.com>
2024-08-23 20:49:58 -0400

    Convertir de un tipo de dato a otro

---
+++ c02_variables_td_datos/p04_convertir.py
@@ -0,0 +1,14 @@
+# Convertir de un tipo de dato a otro
+
+texto = "Hola soy un texto"
+numerito = str(776)
+print(type(numerito))
+# 776
+# "776"
+print(texto + " " + numerito)
+
+numerito = int(776)
+print(type(numerito))
+
+numerito = float(776)
+print(type(numerito))
