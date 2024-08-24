e82e73e22de101f109f8f827f0a0310ae3381707
lmpescobar <lmpes@tutanota.com>
2024-08-23 20:42:26 -0400

    Tipos de datos en Python

---
+++ c02_variables_td_datos/p03_tipos.py
@@ -0,0 +1,25 @@
+# Tipos de datos en Python
+
+nada = None
+cadena = "Hola soy luis"
+cadena = "Estoy aprendiendo python"
+entero = 100
+flotante = 4.0
+booleano = False
+booleano = True
+lista = [10,40,50,80]
+listaString = [44, "treinta", 30, "cuarenta"]
+tuplaNoCambia = ("master", "en", "python")
+diccionario = {
+    "nombre": "Luis",
+    "apellido": "Escobar",
+    "curso": "Master en Python"
+}
+rango = range(9)
+dato_byte = b"Probando"
+
+# Imprimir variable
+print()
+
+# mostrar tipo de dato
+print(type())
