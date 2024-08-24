c138add94595c608010e8d9b3cbb06ecda9dd39d
lmpescobar <lmpes@tutanota.com>
2024-08-24 12:23:36 -0400

    Entrada y salida de datos

---
+++ c04_entrada/p01_entrada_salida.py
@@ -0,0 +1,8 @@
+# Entrada y salida de datos
+
+# Entrada
+nombre = input("¿Cual es tu nombre?: ")
+edad = input("¿Cual es tu edad?: ")
+
+# Salida
+print(f"Me alegro de conocerte, bienvenido {nombre}, veo que tienes {int(edad) + 2} años")
