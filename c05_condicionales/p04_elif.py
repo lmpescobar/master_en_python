011491b1f9b80a3ff64b4ee7f925d5a9c612d77b
lmpescobar <lmpes@tutanota.com>
2024-08-24 18:00:32 -0400

    Elif

---
+++ c05_condicionales/p04_elif.py
@@ -0,0 +1,38 @@
+# Elif
+
+# Ejemplo 4
+print("########## Ejemplo 4 ##########")
+
+dia = int(input("Introduce el numero del dia de la semana: "))
+
+"""
+if dia == 1:
+    print("Es lunes")
+else:
+    if dia == 2:
+        print("Es martes")
+    else:
+        if dia == 3:
+            print("Es miercoles")
+        else:
+            if dia == 4:
+                print("Es jueves")
+            else:
+                if dia == 5:
+                    print("Es viernes")
+                else:
+                    print("Es fin de samana")
+"""
+
+if dia == 1:
+    print("Es lunes")
+elif dia == 2:
+    print("Es martes")
+elif dia == 3:
+    print("Es miercoles")
+elif dia == 4:
+    print("Es jueves")
+elif dia == 5:
+    print("Es viernes")
+else:
+    print("Es fin de samana")
