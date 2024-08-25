61439686868917db7716dd026f0ddb5433e2380a
lmpescobar <lmpes@tutanota.com>
2024-08-25 10:40:33 -0400

    Ejercicio 1

---
+++ c07_ejercicios/p01_ejercicio1.py
@@ -0,0 +1,15 @@
+# Ejercicio 1
+
+"""
+Ejercicio 1.
+    - Crear variables una "pais" y otra "continente"
+    - mostrar su valor por pantalla (Imprimir)
+    - Poner un comentario diciendo el tipo de dato
+
+"""
+
+pais = "Venezuela" # str
+continente = "AméricaDelSur" # str
+year = 2024
+
+print(f"Yo soy de {pais} y {continente} - {str(year)}")
